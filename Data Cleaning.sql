e46bea51233bd1c19fe8469629656d17dbc0ccd3
Afzal Rangrej <rrangrejafzal@gmail.com>
2026-02-21 11:16:22 +0530

    initial comment

---
+++ Data Cleaning.sql
@@ -0,0 +1,13 @@
+-- * Data Cleaning * --
+
+-- Data cleaning in MySQL is about taking messy, inconsistent, or incomplete 
+-- data and making it reliable enough for analysis or apps. Below is a clear, 
+-- practical guide with real SQL patterns you’ll actually use.
+
+SELECT * FROM layoffs;
+
+
+
+
+
+
